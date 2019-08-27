4764919cbae46603c41980edc8b48be1a3a90b2a
Robert Szmurlo <szmurlor@gmail.com>
2019-08-27 18:09:29 +0200

    Dopracowan wersja...

---
+++ deploy-docker-head.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+docker login
+docker pull szmurlor/bdcalc:0.1
+docker 
---
+++ docker/docker-cluster.py
@@ -0,0 +1,18 @@
+import ray
+import time
+import json
+
+@ray.remote
+def getnode():
+    time.sleep(0.1)
+    return ray.services.get_node_ip_address()
+#    return ray.worker.task_context()
+
+ray.init(redis_address="ham-10:6379")
+print(json.dumps(ray.nodes(), indent=4, sort_keys=True))
+print(set(ray.get([getnode.remote() for _ in range(100)])))
+print(ray.state.available_resources())
+print(ray.state.cluster_resources())
+print(ray.state.errors())
+#print(ray.state.objects())
+#print(ray.state.tasks())
