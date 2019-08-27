98c8b58301bb85a70e02807165d72fb4ed1b09ae
Robert Szmurlo <szmurlor@gmail.com>
2019-08-27 13:04:25 +0200

    Working ray cluster - the head uses host networking which allows to expose the ray cluster outside of a docker swarm.

--- docker/start-head.sh
+++ docker/start-head.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-RES=$(ray start --head --node-ip-address ray-head --redis-port 40000 --temp-dir=/bdcalc/ray_session/)
+RES=$(ray start --head --node-ip-address ray-head --redis-port 6379 --temp-dir=/bdcalc/ray_session/)
 sleep infinity
--- docker/start-node.sh
+++ docker/start-node.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-RES=$(ray start --redis-address ray-head:40000 --temp-dir=/bdcalc/ray_session/)
+RES=$(ray start --redis-address ray-head:6379 --temp-dir=/bdcalc/ray_session/)
 sleep infinity
