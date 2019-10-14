c0186140100d9e332b49479b59689471a1b39646
Robert Szmurlo <szmurlor@gmail.com>
2019-10-14 18:04:48 +0200

    Dodalem skryty startujace.

---
+++ run/calc_fantom_non_normalization.sh
@@ -0,0 +1,2 @@
+cd src
+python3 bdcalc.py /doses-nfs/sim/fantom_no_normalization/ -c /doses-nfs/sim/fantom_no_normalization/input/config.json
---
+++ run/calc_pacjent_ray.sh
@@ -0,0 +1,2 @@
+cd src
+python3 bdcalc.py /doses-nfs/sim/pacjent_ray/ -c /doses-nfs/sim/pacjent_ray/input/config.json
