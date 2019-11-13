7b4e33dab3fee05412f7749bd03ad42a278c3591
Robert Szmurlo <szmurlor@gmail.com>
2019-11-13 18:05:11 +0100

    Chcemy znowu policzyc...

--- run/calc_pacjent_ray.sh
+++ run/calc_pacjent_ray.sh
@@ -1,2 +1,8 @@
-cd src
+#!/bin/bash
+
+# To ponizej aby wyciągnąć katlog w którym jest uruchamiany skrypt
+SCRIPT_DIR="$( cd "$(dirname $0)" && pwd)"
+echo "You have started script from folder: $SCRIPT_DIR"
+echo "Starting bdcalc.py from folder: $SCRIPT_DIR/../src/"
+cd $SCRIPT_DIR/../src
 python3 bdcalc.py /doses-nfs/sim/pacjent_ray/ -c /doses-nfs/sim/pacjent_ray/input/config.json
