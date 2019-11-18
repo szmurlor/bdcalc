12aca6a746f8c7b763c5a9c632c4f54e565fc2bb
Robert Szmurlo <szmurlor@gmail.com>
2019-11-18 13:48:55 +0100

    Poprwione startowanie maszyny deweloperskiej lokalnie - teraz ray podpina sie pod lokalna siec i wymienia bez problemu dane z pozostałymi węzłami.

--- docker/docker-cluster.py
+++ docker/docker-cluster.py
@@ -4,13 +4,16 @@ import json
 
 @ray.remote
 def getnode():
-    time.sleep(0.1)
+    #time.sleep(0.1)
     return ray.services.get_node_ip_address()
-#    return ray.worker.task_context()
 
 ray.init(redis_address="ray-0:6379")
+#ray.init(redis_address="172.17.0.2:64940")
 print(json.dumps(ray.nodes(), indent=4, sort_keys=True))
-print(set(ray.get([getnode.remote() for _ in range(100)])))
+id = getnode.remote();
+print(id)
+print(ray.get(id))
+#print(set(ray.get([getnode.remote() for _ in range(1)])))
 print(ray.state.available_resources())
 print(ray.state.cluster_resources())
 print(ray.state.errors())
