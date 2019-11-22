f51045b876720f396255055d5c15d43d294494aa
Robert Szmurlo <szmurlor@gmail.com>
2019-11-22 17:34:42 +0100

    Dodanei pliki startowe tylko.

---
+++ run/calc_pacjent.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ "$#" -lt 1 ]; then
+	echo -e "ERROR!\n\nRequired positional argument is missing: the subfolder of the case to calculate. The correct usage is:\n\t$0 case_name\n"
+	exit 2
+fi
+
+# To ponizej aby wyciągnąć katlog w którym jest uruchamiany skrypt
+SCRIPT_DIR="$( cd "$(dirname $0)" && pwd)"
+echo "You have started script from folder: $SCRIPT_DIR"
+echo "Starting bdcalc.py from folder: $SCRIPT_DIR/../src/"
+
+set -e
+
+cd $SCRIPT_DIR/../src
+python3 /doses-nfs/bdcalc/src/bdcalc.py /doses-nfs/sim/$1/ -c /doses-nfs/sim/$1/input/config.json
