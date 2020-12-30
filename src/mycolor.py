dc9e48e5fb9ffba5c5ca2ff1832adabe461efe81
Robert Szmurlo <robert.szmurlo@ee.pw.edu.pl>
2020-12-30 18:14:03 +0100

    Windows terminal can not do syntax highlighting...

--- src/mycolor.py
+++ src/mycolor.py
@@ -1,13 +1,18 @@
 #!/usr/bin/env python
 import sys
+import os
 
 NC='\033[0m' # No Color
 ORANGE='\033[0;33m'
 RED='\033[0;31m'
 for l in sys.stdin:
-    if "- error -" in l.lower(): 
-       print(f"{RED}{l}{NC}",end='')
-    elif "- warning -" in l.lower(): 
-       print(f"{ORANGE}{l}{NC}",end='')
+
+    if os.name != 'nt':
+        if "- error -" in l.lower(): 
+           print(f"{RED}{l}{NC}",end='')
+        elif "- warning -" in l.lower(): 
+           print(f"{ORANGE}{l}{NC}",end='')
+        else:
+           print(l,end='')
     else:
-       print(l,end='')
+        print(l,end='')
